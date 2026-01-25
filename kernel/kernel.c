37edef7d2da699ec4bfef5cef347767349f2409c
Trollycat <kittygamingbuisness@gmail.com>
2026-01-25 16:25:21 -0500

    Text

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -14,20 +14,22 @@ void kmain(void)
 {
     vga_init();
 
+    /*
+     * For now we display some fake information.
+     */
     vga_set_color(COLOR_LIGHT_GREEN, COLOR_BLACK);
-    puts("Initializing kernel subsystems...\n");
-    puts("[ OK ] GDT & IDT Loaded");
-    puts("[ OK ] Physical Memory Manager Active");
-    puts("[ OK ] Kernel Heap Initialized");
-    puts("[ OK ] VFS Root Mounted");
-    puts("[ OK ] PIC & Timer Calibrated (1000Hz)");
-    puts("[ OK ] Keyboard Controller Ready");
+    puts("[ OK ] Initalizing Kernel...");
+    puts("[ OK ] Initalizing Memory...");
+    puts("[ OK ] Initalizing Hardware...");
+    puts("[ OK ] Initalizing Drivers...");
+    puts("[ OK ] Initalizing Libraries...");
+    puts("[ OK ] Initalizing Timer...");
+    puts("[ OK ] Initalizing Shell...");
+    puts("[ OK ] Initalizing Text...");
 
-    vga_set_color(COLOR_LIGHT_GREEN, COLOR_BLACK);
-    puts("\nSystem Information: ");
-
-    puts("\n[NAME]: Thuban");
-    puts("[VERION]: 0.1");
+    vga_set_color(COLOR_LIGHT_GREEN | COLOR_LIGHT_RED, COLOR_BLACK);
+    puts("\n--- System Information: ---");
+    puts("STATUS: OK");
 
     while (1)
     {
